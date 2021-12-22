c1898ae9b3988d0e5f6a1c799916758c733f6d17
placoderm <31570884+placoderm@users.noreply.github.com>
2021-12-22 15:03:35 +0530

    Delete src directory

--- src/faviconTitle.js
+++
@@ -1,8 +0,0 @@
-export default function faviconTitle(favImg, title) {
-  let headTitle = document.querySelector("head");
-  let setFavicon = document.createElement("link");
-  setFavicon.setAttribute("rel", "shortcut icon");
-  setFavicon.setAttribute("href", favImg);
-  headTitle.appendChild(setFavicon);
-  document.title = title;
-}
