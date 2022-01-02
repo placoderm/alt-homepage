6de77170927f3500d1356e9be91b9e89a7c9e1ea
placoderm <31570884+placoderm@users.noreply.github.com>
2022-01-02 11:00:14 +0530

    Delete src directory

--- src/webSites/createAncientBuddhistTextsLink.test.js
+++
@@ -1,7 +0,0 @@
-import createAncientBuddhistTextsLink from "./createAncientBuddhistTextsLink.js";
-
-test("test all mn141 citation", () => {
-  expect(
-    createAncientBuddhistTextsLink({ site: "ABT", book: "mn", firstNumber: 141, secondNumber: 0, error: "" })
-  ).toBe("https://www.ancient-buddhist-texts.net/Texts-and-Translations/Short-Pieces/Saccavibhangasuttam.htm");
-});
